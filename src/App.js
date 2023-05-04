4b8e6d146e4113e9954ebd4e43cdef8f2616d55c
moonk <mkjoehsb@gmail.com>
2023-05-05 08:05:49 +0900

    challenge ing.....

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { useEffect, useState } from "react";
 function App() {
   const [loading, setLoading] = useState(true);
   const [coins, setCoins] = useState([]);
+  const [value, setValue] = useState(0);
 
   useEffect(() => {
     fetch("https://api.coinpaprika.com/v1/tickers")
@@ -11,17 +12,29 @@ function App() {
     setLoading(false);
   }, []);
 
+  const onChange = (event) => {
+    setValue(event.target.value);
+  };
   return (
     <div>
       <h1>The Coins ({coins.length})</h1>
       {loading ? <strong> Loading...</strong> : null}
-      <ul>
+
+      <input
+        onChange={onChange}
+        value={value}
+        type="text"
+        placeholder="how much?"
+      />
+      <select>
         {coins.map((coin) => (
-          <li>
-            {coin.name} ({coin.symbol}) : {coin.quotes.USD.price}
-          </li>
+          <option>
+            {coin.name} ({coin.symbol}) : ${coin.quotes.USD.price}USD
+          </option>
         ))}
-      </ul>
+      </select>
+      <button> Submit </button>
+      <h3>You can buy </h3>
     </div>
   );
 }
