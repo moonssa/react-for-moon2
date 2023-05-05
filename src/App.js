0f1419adb73b6c4fb996ffe66a1a036d6577513f
moonk <mkjoehsb@gmail.com>
2023-05-05 11:09:33 +0900

    movie app part1

--- src/App.js
+++ src/App.js
@@ -3,19 +3,42 @@ import { useEffect, useState } from "react";
 function App() {
   const [loading, setLoading] = useState(true);
   const [movies, setMovies] = useState([]);
-  useEffect(() => {
-    fetch(
+  const getMovies = async () => {
+    const response = fetch(
       "https://yts.mx/api/v2/list_movies.json?minimum_rating=9&sort_by=year",
-    )
-      .then((response) => response.json())
-      .then((json) => {
-        setMovies(json.data.movies);
-        setLoading(false);
-      });
+    );
+    const json = (await response).json();
+
+    setMovies(json.data.movies);
+    setLoading(false);
+  };
+  useEffect(() => {
+    getMovies();
   }, []);
 
   console.log(movies);
 
-  return <div>{loading ? <h1> Loading</h1> : null}</div>;
+  return (
+    <div>
+      {loading ? (
+        <h1> Loading</h1>
+      ) : (
+        <div>
+          {movies.map((movie) => (
+            <div key={movie.id}>
+              <h2>{movie.title}</h2>
+              <img src={movie.medium_cover_image} />
+              <p>{movie.summary}</p>
+              <ul>
+                {movie.genres.map((g) => (
+                  <li key={g}>{g}</li>
+                ))}
+              </ul>
+            </div>
+          ))}
+        </div>
+      )}
+    </div>
+  );
 }
 export default App;
