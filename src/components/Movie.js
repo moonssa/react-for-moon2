0aa5a0d9bc4fe64505cc759119688dafca9a9f43
moonk <mkjoehsb@gmail.com>
2023-05-06 10:47:28 +0900

    react router

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,10 +1,13 @@
 import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
 
 function Movie({ title, coverImg, summary, genres }) {
   return (
     <div>
-      <h2>{title}</h2>
-      <img src={coverImg} />
+      <h2>
+        <Link to="/movie"> {title} </Link>
+      </h2>
+      <img src={coverImg} alt={title} />
       <p>{summary}</p>
       <ul>
         {genres.map((g) => (
--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,14 +1,15 @@
 import { useEffect, useState } from "react";
-import Movie from "./components/Movie";
+import Movie from "../components/Movie";
 
 function Home() {
   const [loading, setLoading] = useState(true);
   const [movies, setMovies] = useState([]);
   const getMovies = async () => {
-    const response = fetch(
-      "https://yts.mx/api/v2/list_movies.json?minimum_rating=9&sort_by=year",
-    );
-    const json = (await response).json();
+    const json = await (
+      await fetch(
+        "https://yts.mx/api/v2/list_movies.json?minimum_rating=9&sort_by=year",
+      )
+    ).json();
 
     setMovies(json.data.movies);
     setLoading(false);
@@ -22,7 +23,7 @@ function Home() {
   return (
     <div>
       {loading ? (
-        <h1> Loading</h1>
+        <h1> Loading.....</h1>
       ) : (
         <div>
           {movies.map((movie) => (
