3b7953ecf5c26bd900786caf1c63eda975dcd635
moonk <mkjoehsb@gmail.com>
2023-05-06 14:24:56 +0900

    git detail page

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,19 +1,31 @@
 import { useParams } from "react-router-dom";
-import { useEffect } from "react";
+import { useState, useEffect } from "react";
 
 function Detail() {
   const { id } = useParams();
-
+  const [movie, setMovie] = useState("");
   const getMovie = async () => {
     const json = await (
       await fetch(`https://yts.mx/api/v2/movie_details.json?movie_id=${id}`)
     ).json();
+    setMovie(json.data.movie);
+    console.log(json.data.movie);
     console.log(json.data.movie.url);
   };
   useEffect(() => {
     getMovie();
   }, []);
 
-  return <h1> Detail </h1>;
+  console.log("hahaha", movie);
+  return (
+    <div>
+      <h1>{movie.title} </h1>
+      <h2>⭐️ : {movie.rating}</h2>
+      <img src={movie.medium_cover_image} />
+      <div>
+        <a href={movie.url}> {movie.title} 홈페이지 바로가기</a>
+      </div>
+    </div>
+  );
 }
 export default Detail;
