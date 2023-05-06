309c5774891f51681eb86860fceb1584b1dd9aba
moonk <mkjoehsb@gmail.com>
2023-05-06 12:53:32 +0900

    using parameter

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -28,6 +28,7 @@ function Home() {
         <div>
           {movies.map((movie) => (
             <Movie
+              id={movie.id}
               key={movie.id}
               title={movie.title}
               coverImg={movie.medium_cover_image}
